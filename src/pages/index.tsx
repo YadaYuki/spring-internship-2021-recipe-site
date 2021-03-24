abc9d354408c7acd016849f58ae4057015cfb7a1
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-03-24 16:22:43 +0900

    Add Logic for Search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,32 @@ import { Recipe } from "../types";
 import { NextPage } from "next";
 import Link from "next/link";
 import Header from "../components/header";
+import {SearchPageQuery} from "./query-type"
 
-interface Props{
-  query:any
+interface Props {
+  query: SearchPageQuery;
 }
 
-const SearchPage: NextPage<Props> = ({query}) => {
+const SearchPage: NextPage<Props> = ({ query }) => {
   const [recipes, setRecipes] = useState<Recipe[] | null>(null);
-  const {q} = query
+  const { q, page } = query;
+  const getLatestRecipes = async () => {
+    const data = await api.getRecipes(page);
+    return data;
+  };
+  const searchRecipes = async () => {
+    const data = await api.searchRecipes(q, page);
+    return data;
+  };
   useEffect(() => {
-    const getRecipes = async () => {
-      const data = await api.getRecipes();
-      return data;
-    };
-    console.log(q)
+    const getRecipes = q === undefined ? getLatestRecipes : searchRecipes;
     getRecipes()
       .then((data) => {
         const { recipes } = data;
         setRecipes(recipes);
       })
       .catch((err: any) => {});
-  });
+  }, []);
   return (
     <div>
       <Header />
