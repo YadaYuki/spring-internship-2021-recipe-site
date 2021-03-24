818cedc7a27cc60b0cfd9fe48fe2305e9a81feb8
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-03-24 15:48:33 +0900

    Add Drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,44 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import React from "react";
-import Logo from "./Logo.svg";
-import { Button } from 'antd';
+import React,{useState,useCallback} from "react";
+import TitleLogo from "../../public/title.svg";
+import SearchLogo from "../../public/search.svg";
+import { Button } from "antd";
+import SearchDrawer from "./drawer"
 
 interface Props {}
 
 const Header: React.FC<Props> = () => {
+  const [drawerIsOpen,setDrawerIsOpen] = useState<boolean>(false)
+  const handleOpen = useCallback(()=>{
+    setDrawerIsOpen(true)
+  },[])
+  const handleClose = useCallback(()=>{
+    setDrawerIsOpen(false)
+  },[])
   return (
-    <div css={css`background:black;"`}>
-      <Logo />
-      <Button type="primary">hogehgoe</Button>
+    <div css={HeaderStyle}>
+      <TitleLogo />
+      <Button onClick={handleOpen} type="text" shape="circle" icon={<SearchLogo />} />
+      <SearchDrawer handleClose={handleClose} visible={drawerIsOpen} />
     </div>
   );
 };
+
+const HeaderStyle = css`
+  background: #e3c006;
+  height: 48px;
+  position: fixed;
+  width: 100%;
+  > svg {
+    margin-top: 8px;
+    margin-left: 8px;
+  }
+  >button{
+    position: fixed;
+    right:8px;
+    top:8px;
+  }
+`;
 
 export default Header;
