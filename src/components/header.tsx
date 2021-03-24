0975f5cb82a632d5b7afac57ed13b79f27798041
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-03-24 14:56:41 +0900

    Add Global Style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 import { css } from "@emotion/react";
 import React from "react";
 import Logo from "./Logo.svg";
+import { Button } from 'antd';
 
 interface Props {}
 
@@ -9,6 +10,7 @@ const Header: React.FC<Props> = () => {
   return (
     <div css={css`background:black;"`}>
       <Logo />
+      <Button type="primary">hogehgoe</Button>
     </div>
   );
 };
